98ff8b5e1d9c77d78022df5759388e0190f6eb1f
wesleyabreeuu <wesleyavivabr@gmail.com>
2025-10-07 22:44:37 -0300

    configuração deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repo = "portfolio"; 
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",                 
+  images: { unoptimized: true },    
+  basePath: isProd ? `/${repo}` : undefined,
+  assetPrefix: isProd ? `/${repo}/` : undefined,
+  // opcional: trailingSlash: true,
 };
 
 export default nextConfig;
