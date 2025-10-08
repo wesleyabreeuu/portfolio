cbdabd8286d490d8043174742f994a8446d6e85c
wesleyabreeuu <wesleyavivabr@gmail.com>
2025-10-07 23:02:23 -0300

    ci: não falhar build por ESLint (ignoreDuringBuilds)

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
-const repo = "portfolio"; 
+const repo = "portfolio"; // seu repositório
 
 const nextConfig: NextConfig = {
-  output: "export",                 
-  images: { unoptimized: true },    
+  output: "export",
+  images: { unoptimized: true },
   basePath: isProd ? `/${repo}` : undefined,
   assetPrefix: isProd ? `/${repo}/` : undefined,
-  // opcional: trailingSlash: true,
+
+  eslint: { ignoreDuringBuilds: true }, // <- NÃO trava o build por lint
 };
 
 export default nextConfig;
